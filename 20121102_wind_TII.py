5117aedd44a6bdfbd5998754cc5344e800e258dd
windri06 <121286693+windri06@users.noreply.github.com>
2022-12-25 19:14:50 +0700

    Add files via upload

---
+++ 20121102_wind_TII.py
@@ -0,0 +1,45 @@
+
+
+# soal pertama
+print ( 'Jawaban Pertama' )
+cetak ( '' )
+data_mahasiswa  = {
+    'NIM' : '20121102' ,
+    'Nama' : 'windri' ,
+    'Kelas' : 'TI3A' ,
+    'Alamat' : 'Tulis Batang'
+}
+print ( "data_mahasiswa = " , data_mahasiswa )
+
+# soal kedua
+print ( 'Jawaban Kedua' )
+cetak ( '' )
+cetak ( '-------------------------------------------------- -' )
+print ( 'Aplikasi Sederhana Menggunakan String Dan Array' )
+cetak ( '-------------------------------------------------- -' )
+print ( 'NIM : ' , data_mahasiswa [ 'NIM' ])
+print ( 'Nama : ' , data_mahasiswa [ 'Nama' ])
+print ( 'Kelas : ' , data_mahasiswa [ 'Kelas' ])
+print ( 'Alamat : ' , data_mahasiswa [ 'Alamat' ])
+cetak ( '-------------------------------------------------- -' )
+
+# soal ketiga
+print ( 'Jawaban Ketiga' )
+cetak ( '' )
+kalimat  = [ 'UNISS' , 'di' , 'belajar' , 'pada' , 'Mahasiswa' , 'data' , 'ruang' , 'lab' , 'struktur' , 'semester' , 'tema' , 'Array' , 'String' , 'dan' , 'di' , 'dengan' , 'Batang' , '3' ]
+
+cetak ( kalimat [ 4 ], kalimat [ 9 ], kalimat [ 17 ], kalimat [ 1 ], kalimat [ 0 ], kalimat [ 16 ], kalimat [ 2 ], kalimat [ 8 ], kalimat [ 5 ], kalimat [ 3 ], kalimat [ 6 ], kalimat [ 7 ], kalimat [ 15 ],kalimat [ 10 ], kalimat [ 12 ], kalimat [ 13 ], kalimat [ 11 ])
+
+# soal keempat
+print ( 'Jawaban Keempat' )
+cetak ( '' )
+dari  impor array  * 
+Nilai  =  array ( 'i' ,[ 1 , 3 , 4 , 5 , 10 , 15 , 20 ])
+
+cetak ( '-------------------------------------------------- --' )
+print ( 'Aplikasi Sederhan menggunakan Array' )
+cetak ( '-------------------------------------------------- --' )
+print ( Nilai [ 4 ], ' + ' , Nilai [ 1 ], ' x ' , Nilai [ 5 ], ' = ' , Nilai [ 4 ] + Nilai [ 1 ] * Nilai [ 5 ])
+print ( Nilai [ 1 ], ' x ' , Nilai [ 2 ], ' : 2 = ' , Nilai [ 1 ] * Nilai [ 2 ] / 2 )
+print ( Nilai [ 5 ], '^2 x ' , Nilai [ 1 ], ' + ' , Nilai [ 3 ], ' = ' , Nilai [ 5 ] ** 2  * Nilai [ 1 ] + Nilai [ 3 ])
+print ( Nilai [ 5 ], ' - ' , Nilai [ 3 ], ' x ' , Nilai [ 2 ], ' = ' , Nilai [ 5 ] - Nilai [ 3 ] * Nilai [ 2 ])
